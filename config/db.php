621b6719e9bcde9945eba34b565f95061a79406d
Vadim <wesborlandzp@gmail.com>
2018-11-28 21:10:32 +0200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 $config['settings']['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
 
-$config['db']['host']   = 'localhost';
+$config['db']['host']   = 'https://social-network-zp.herokuapp.com';
 $config['db']['user']   = 'root';
 $config['db']['pass']   = '';
 $config['db']['dbname'] = 'socialnetwork';
